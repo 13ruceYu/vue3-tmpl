a7d492f76ea5469451ef04533bb19b0d55a8a3fc
13ruceYu <bruceyuhongbo@gmail.com>
2022-07-25 00:16:16 +0800

    chore: eslint config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue'
+import App from './App.vue'
 import router from './router/index'
 
 import './style/index.less'
