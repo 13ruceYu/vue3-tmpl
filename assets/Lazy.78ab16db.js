08308b983e394f8e2a8e4365bb261bacd78514de
13ruceYu <13ruceYu@users.noreply.github.com>
2022-07-28 16:47:14 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 1c15abe508664a13a6006ff5abda9143a473b49f

---
+++ assets/Lazy.78ab16db.js
@@ -0,0 +1,1 @@
+import{d as a,r as e,t as r}from"./index.4006cae9.js";const p=a({__name:"Lazy",setup(s){const t=e("Lazy");return(n,o)=>r(t.value)}});export{p as default};
--- assets/Lazy.843e94d8.js
+++
@@ -1,1 +0,0 @@
-import{d as a,r as e,t as r}from"./index.4a5edb8a.js";const p=a({__name:"Lazy",setup(s){const t=e("Lazy");return(n,o)=>r(t.value)}});export{p as default};
