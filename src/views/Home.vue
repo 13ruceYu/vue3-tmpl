1c15abe508664a13a6006ff5abda9143a473b49f
13ruceYu <bruceyuhongbo@gmail.com>
2022-07-29 00:46:05 +0800

    chore: use unocss preset icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,11 +14,20 @@
 </script>
 
 <template>
-  <div class="text-center">
-    <h2 class="c-pink">yo, {{ user.username }}</h2>
+  <div class="text-center" mb-1>
+    <div class="c-orange text-sm">hola, {{ user.username }}</div>
   </div>
-  <p text-center c-cyan>
-    <em text-sm opacity-50>use unocss</em><br />
-    <a-button type="primary">ant design button</a-button>
-  </p>
+  <div text-center>
+    <a-button type="primary">ant design button</a-button><br />
+  </div>
+  <div class="flex text-xl items-center justify-center">
+    <div class="mr-1 text-2xl italic c-pink-3"> h </div>
+    <div i-carbon:cloudy class="c-blue-5"></div>
+    <div i-carbon:palm-tree class="c-green-6"></div>
+    <div i-carbon:palm-tree class="c-green-6"></div>
+    <div i-carbon:sun class="c-yellow inline-block"></div>
+  </div>
+  <div text-center>
+    <em class="text-sm">powered by unocss</em>
+  </div>
 </template>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,13 +1,4 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetTypography,
-  presetUno,
-  presetWebFonts,
-  transformerDirectives,
-  transformerVariantGroup
-} from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [
@@ -16,16 +7,6 @@ export default defineConfig({
     presetIcons({
       scale: 1.2,
       warn: true
-    }),
-    presetTypography(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono'
-      }
     })
-  ],
-  transformers: [transformerDirectives(), transformerVariantGroup()],
-  safelist: 'prose prose-sm m-auto text-left'.split(' ')
+  ]
 })
